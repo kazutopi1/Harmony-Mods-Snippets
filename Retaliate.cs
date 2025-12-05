5f68a498250eb85c95169a9afea2b9654cc79c24
Kazu Topi <kazutopi1@gmail.com>
2025-12-05 18:19:15 +0800

    Create Retaliate.cs

---
+++ Retaliate.cs
@@ -0,0 +1,54 @@
+using StardewValley;
+using StardewValley.Buffs;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using MonoGame.Framework;
+using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Atttttackk
+{
+    public class ThisWillWorkIPromise : Mod
+    {
+        private const int Threshhold = 50;
+
+        private static int hitCount = 0;
+
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Farmer), nameof(Farmer.takeDamage)),
+                postfix: new HarmonyMethod(typeof(ThisWillWorkIPromise), nameof(ThisWillWorkIPromise.Postfix))
+            );
+        }
+        private static Buff BuffSetup()
+        {
+            Buff Retaliate = new Buff(
+                id: "df.retaliate",
+                displayName: "Retaliate",
+                iconTexture: Game1.content.Load<Texture2D>("TileSheets/BuffsIcons"),
+                iconSheetIndex: 20,
+                duration: 1_000,
+                effects: new BuffEffects()
+                {
+                    CriticalPowerMultiplier = { 2 },
+                    CriticalChanceMultiplier = { 1000 },
+                    WeaponSpeedMultiplier = { 10 },
+                }
+            );
+            return Retaliate;
+        }
+        private static void Postfix(Farmer __instance, int damage)
+        {
+            hitCount += 1;
+
+            if (hitCount >= Threshhold)
+            {
+                __instance.applyBuff(BuffSetup());
+                hitCount = 0;
+            }
+        }
+    }
+}
